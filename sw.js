79e0231184b91891e9e0ab7efc1dba79d6dcbb19
postsup2077 <jonlouismilano@gmail.com>
2026-03-29 07:23:36 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Kill switch - unregister self and clear all caches
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
+  );
+});
